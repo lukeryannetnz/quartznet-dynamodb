48f107dab39bba143c06388860d44672ad41d21d
lukeryannetnz <github@lukeryan.net.nz>
2016-02-25 21:02:36 +1300

    Starting calendar interval trigger tests

---
+++ src/QuartzNET-DynamoDB.Tests/Unit/TriggerConverterCalendarIntervalTriggerTests.cs
@@ -0,0 +1,20 @@
+﻿using Quartz.Impl.Triggers;
+
+namespace Quartz.DynamoDB.Tests.Unit
+{
+    public class TriggerConverterCalendarIntervalTriggerTests
+    {
+        public CalendarIntervalTriggerImpl CreateTrigger()
+        {
+            CalendarIntervalTriggerImpl trigger = (CalendarIntervalTriggerImpl) TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "myTriggerGroup")
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInHours(1)
+                    .WithRepeatCount(100))
+                .StartAt(DateBuilder.FutureDate(10, IntervalUnit.Minute))
+                .Build();
+
+            return trigger;
+        }
+    }
+}

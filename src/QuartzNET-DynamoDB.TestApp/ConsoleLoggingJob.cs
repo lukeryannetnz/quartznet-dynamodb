ab21dde52a6bd13ba7139347b4bd778f77d782a9
Luke Ryan <hello@lukeryan.net.nz>
2016-05-29 15:48:00 +1200

    Added basic test console app. Uses RAMStore for now.

---
+++ src/QuartzNET-DynamoDB.TestApp/ConsoleLoggingJob.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Quartz;
+
+namespace QuartzNETDynamoDB.TestApp
+{
+	public class ConsoleLoggingJob : IJob
+	{
+		public ConsoleLoggingJob ()
+		{
+		}
+
+
+		public void Execute(IJobExecutionContext context)
+		{
+			Console.WriteLine("ConsoleLoggingJob executing. {0}", DateTime.Now);
+		}
+	}
+}
+
---
+++ src/QuartzNET-DynamoDB.TestApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Quartz;
+using Quartz.Impl;
+using System.Threading;
+
+namespace QuartzNETDynamoDB.TestApp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			PrintGreeting();
+
+			// Grab the Scheduler instance from the Factory 
+			IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+			// and start it off
+			scheduler.Start();
+
+			ScheduleConsoleLoggingJob(scheduler);
+
+			while (Console.ReadKey().Key != ConsoleKey.Q)
+			{
+				Thread.Sleep(100);
+			}
+
+			// and last shut down the scheduler when you are ready to close your program
+			scheduler.Shutdown();
+		}
+
+		static void PrintGreeting()
+		{
+			Console.WriteLine("---------------------------------------");
+			Console.WriteLine("Welcome to QuartzNET-DynamoDB test app!");
+			Console.WriteLine("Press Q to quit.");
+			Console.WriteLine("---------------------------------------");
+		}
+
+		private static void ScheduleConsoleLoggingJob(IScheduler scheduler)
+		{
+			// define the job and tie it to our HelloJob class
+			IJobDetail job = JobBuilder.Create<ConsoleLoggingJob>()
+				.WithIdentity("ConsoleJob", "ConsoleJobsGroup")
+				.Build();
+
+			// Trigger the job to run now, and then repeat every 10 seconds
+			ITrigger trigger = TriggerBuilder.Create()
+				.WithIdentity("FiveSecondTrigger", "SimpleTriggersGroup")
+				.StartNow()
+				.WithSimpleSchedule(x => x
+					.WithIntervalInSeconds(5)
+					.RepeatForever())
+				.Build();
+
+			// Tell quartz to schedule the job using our trigger
+			scheduler.ScheduleJob(job, trigger);
+		}
+	}
+}

fb61d44d18b61a81e893bea3bd1332d9fab7bb36
Luke Ryan <hello@lukeryan.net.nz>
2016-07-17 19:36:45 +1200

    Test for basecalendar serialisation

---
+++ src/QuartzNET-DynamoDB.Tests/Unit/CalendarSerialisationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Quartz.Impl.Calendar;
+using Xunit;
+
+namespace Quartz.DynamoDB.Tests
+{
+    /// <summary>
+    /// Tests the DynamoCalendar serialisation for all quartz derived calendar types.
+    /// </summary>
+    public class CalendarSerialisationTests
+    {
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void BaseCalendarDescription()
+        {
+            BaseCalendar cal = new BaseCalendar() { Description = "Hi mum" };
+
+            var sut = new DynamoCalendar("test", cal);
+            var serialised = sut.ToDynamo();
+            var deserialised = new DynamoCalendar(serialised);
+
+            Assert.Equal(sut.Description, deserialised.Description);
+        }
+    }
+}
+

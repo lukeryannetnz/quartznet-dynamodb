e9ad00430b1f991841b7371dce51e0bc80f868cb
Luke Ryan <hello@lukeryan.net.nz>
2016-07-17 15:19:47 +1200

    Renamed getjobtests to be singluar. Added test class for get triggers.

---
+++ src/QuartzNET-DynamoDB.Tests/Integration/JobStore/GetTriggerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Quartz.Simpl;
+using Quartz.Spi;
+
+namespace Quartz.DynamoDB.Tests
+{
+	public class GetTriggerTests
+	{
+		IJobStore _sut;
+
+		public GetTriggerTests ()
+		{
+			_sut = new JobStore ();
+			var signaler = new Quartz.DynamoDB.Tests.Integration.RamJobStoreTests.SampleSignaler ();
+			var loadHelper = new SimpleTypeLoadHelper ();
+
+			_sut.Initialize (loadHelper, signaler);
+		}
+	}
+}
+

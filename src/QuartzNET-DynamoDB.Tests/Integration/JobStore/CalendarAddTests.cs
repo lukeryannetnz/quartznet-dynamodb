b347b8ad082f7fd8c5a305004d5342147abb5479
Luke Ryan <hello@lukeryan.net.nz>
2016-07-17 15:47:16 +1200

    Added test file for calendar

---
+++ src/QuartzNET-DynamoDB.Tests/Integration/JobStore/CalendarAddTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Quartz.Simpl;
+using Quartz.Spi;
+
+namespace Quartz.DynamoDB.Tests
+{
+	public class CalendarAddTests
+	{
+		IJobStore _sut;
+
+		public CalendarAddTests ()
+		{
+			_sut = new JobStore ();
+			var signaler = new Quartz.DynamoDB.Tests.Integration.RamJobStoreTests.SampleSignaler ();
+			var loadHelper = new SimpleTypeLoadHelper ();
+
+			_sut.Initialize (loadHelper, signaler);
+		}
+	}
+}
+

e63929778dd594b30814fbeb54a4bede0e35ae76
Luke Ryan <hello@lukeryan.net.nz>
2016-07-17 14:17:45 +1200

    Added broken job tests

---
+++ src/QuartzNET-DynamoDB.Tests/Integration/JobStoreGetJobsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Quartz.Simpl;
+using Quartz.Spi;
+using Xunit;
+
+namespace Quartz.DynamoDB.Tests
+{
+	/// <summary>
+	/// Contains tests related to retrieving Jobs and Job Groups.
+	/// </summary>
+	public class JobStoreGetJobTests
+	{
+		IJobStore _sut;
+
+		public JobStoreGetJobTests ()
+		{
+			_sut = new JobStore ();
+			var signaler = new Quartz.DynamoDB.Tests.Integration.RamJobStoreTests.SampleSignaler ();
+			var loadHelper = new SimpleTypeLoadHelper ();
+
+			_sut.Initialize (loadHelper, signaler);
+		}
+
+		/// <summary>
+		/// Tests that when no jobs exist, zero is returned when GetNumberOfJobs is called.
+		/// </summary>
+		/// <returns>The number of jobs returns zero.</returns>
+		[Fact]
+		[Trait ("Category", "Integration")]
+		public void GetNumberOfJobsReturnsZero ()
+		{
+			var jobCount = _sut.GetNumberOfJobs();
+
+			Assert.Equal (0, jobCount);
+		}
+	}
+}
+
---
+++ src/QuartzNET-DynamoDB.Tests/Integration/JobStoreJobAddTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Quartz.Impl;
+using Quartz.Job;
+using Quartz.Simpl;
+using Quartz.Spi;
+using Xunit;
+
+namespace Quartz.DynamoDB.Tests
+{
+	/// <summary>
+	/// Contains tests related to the Addition of Jobs and Job Groups.
+	/// </summary>
+	public class JobStoreJobAddTests
+	{
+		IJobStore _sut;
+
+		public JobStoreJobAddTests ()
+		{
+			_sut = new JobStore ();
+			var signaler = new Quartz.DynamoDB.Tests.Integration.RamJobStoreTests.SampleSignaler ();
+			var loadHelper = new SimpleTypeLoadHelper ();
+
+			_sut.Initialize (loadHelper, signaler);
+		}
+
+		/// <summary>
+		/// Tests that when CheckExit is called for a job that exists, it returns true.
+		/// </summary>
+		[Fact]
+		[Trait ("Category", "Integration")]
+		public void CheckExistsWhenJobExsits ()
+		{
+			CreateTestJob ();
+
+		}
+
+		JobDetailImpl CreateTestJob ()
+		{
+			string jobGroup = Guid.NewGuid ().ToString ();
+			// Create a random job, store it.
+			string jobName = Guid.NewGuid ().ToString ();
+			JobDetailImpl detail = new JobDetailImpl (jobName, jobGroup, typeof (NoOpJob));
+			_sut.StoreJob (detail, false);
+
+			return detail;
+		}
+	}
+}
+

c933bf70dfc3487d5013fef6ab3d4f87dd000a2b
Luke Ryan <hello@lukeryan.net.nz>
2016-07-17 19:42:38 +1200

    Add unit test for annual calendar

--- src/QuartzNET-DynamoDB.Tests/Unit/CalendarSerialisationTests.cs
+++ src/QuartzNET-DynamoDB.Tests/Unit/CalendarSerialisationTests.cs
@@ -9,6 +9,10 @@ namespace Quartz.DynamoDB.Tests
     /// </summary>
     public class CalendarSerialisationTests
     {
+        /// <summary>
+        /// Tests that the description property of the base calendar type serialises and deserialises correctly.
+        /// </summary>
+        /// <returns>The calendar description.</returns>
         [Fact]
         [Trait("Category", "Unit")]
         public void BaseCalendarDescription()
@@ -21,6 +25,26 @@ namespace Quartz.DynamoDB.Tests
 
             Assert.Equal(sut.Description, deserialised.Description);
         }
+
+        /// <summary>
+        /// Tests that the excluded days property of the annual calendar serialises and deserialises correctly.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Annual()
+        {
+            var importantDate = new DateTime(2015, 04, 02);
+
+            AnnualCalendar cal = new AnnualCalendar();
+            cal.SetDayExcluded(DateTime.Today, true);
+            cal.SetDayExcluded(importantDate, true);
+
+            var sut = new DynamoCalendar("test", cal);
+            var serialised = sut.ToDynamo();
+            var deserialised = new DynamoCalendar(serialised);
+
+            Assert.True(((AnnualCalendar)deserialised.Calendar).IsDayExcluded(DateTime.Today));
+            Assert.True(((AnnualCalendar)deserialised.Calendar).IsDayExcluded(importantDate));
+        }
     }
 }
-

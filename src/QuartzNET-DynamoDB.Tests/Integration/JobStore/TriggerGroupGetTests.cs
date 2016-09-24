ea0ea9352a40f096ee1aa535f9211f5e346de6ae
Luke Ryan <hello@lukeryan.net.nz>
2016-09-24 16:14:56 +1200

    Add trigger group get tests class

---
+++ src/QuartzNET-DynamoDB.Tests/Integration/JobStore/TriggerGroupGetTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Quartz.Simpl;
+using Quartz.Spi;
+
+namespace Quartz.DynamoDB.Tests
+{
+    public class TriggerGroupGetTests
+    {
+        IJobStore _sut;
+
+        public TriggerGroupGetTests()
+        {
+            _sut = new JobStore();
+            var signaler = new Quartz.DynamoDB.Tests.Integration.RamJobStoreTests.SampleSignaler();
+            var loadHelper = new SimpleTypeLoadHelper();
+
+            _sut.Initialize(loadHelper, signaler);
+        }
+    }
+}
+

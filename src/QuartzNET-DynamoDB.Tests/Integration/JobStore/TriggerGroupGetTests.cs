ff1b1549eb83df817e3b068018cb94f0ceecf3ba
Luke Ryan <hello@lukeryan.net.nz>
2016-09-24 16:36:17 +1200

    Add failing test for get paused trigger groups

--- src/QuartzNET-DynamoDB.Tests/Integration/JobStore/TriggerGroupGetTests.cs
+++ src/QuartzNET-DynamoDB.Tests/Integration/JobStore/TriggerGroupGetTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Quartz.Simpl;
 using Quartz.Spi;
+using Xunit;
 
 namespace Quartz.DynamoDB.Tests
 {
@@ -15,6 +16,22 @@ namespace Quartz.DynamoDB.Tests
             var loadHelper = new SimpleTypeLoadHelper();
 
             _sut.Initialize(loadHelper, signaler);
+        }
+
+        /// <summary>
+        /// Get paused trigger groups returns one record.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void GetPausedTriggerGroupReturnsOneRecord()
+        {
+            //create a trigger group by calling for it to be paused.
+            string triggerGroup = Guid.NewGuid().ToString();
+            _sut.PauseTriggers(Quartz.Impl.Matchers.GroupMatcher<TriggerKey>.GroupEquals(triggerGroup));
+
+            var result = _sut.GetPausedTriggerGroups();
+
+            Assert.True(result.Contains(triggerGroup));
         }
     }
 }

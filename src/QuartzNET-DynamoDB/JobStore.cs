8e75dfb70544897dcf67a4a259f5768964344f6e
Dhruv Dhir <dhruvdhir11@gmail.com>
2016-11-05 11:48:07 +1300

    Implement ResumeAll

--- src/QuartzNET-DynamoDB/JobStore.cs
+++ src/QuartzNET-DynamoDB/JobStore.cs
@@ -922,7 +922,15 @@ namespace Quartz.DynamoDB
 
         public void ResumeAll()
         {
-            throw new NotImplementedException();
+            lock (lockObject)
+            {
+                var triggerGroupNames = GetTriggerGroupNames();
+
+                foreach (var groupName in triggerGroupNames)
+                {
+                    ResumeTriggers(GroupMatcher<TriggerKey>.GroupEquals(groupName));
+                }
+            }
         }
 
         /// <summary>

0845c28871b3b1234c0721afdaf434bf579d2808
Luke Ryan <hello@lukeryan.net.nz>
2016-11-05 22:08:08 +1300

    Run tests with no app domain and for now just always use local dynamo

--- src/QuartzNET-DynamoDB/DynamoDbClientFactory.cs
+++ src/QuartzNET-DynamoDB/DynamoDbClientFactory.cs
@@ -9,17 +9,17 @@ namespace Quartz.DynamoDB
     {
         public static AmazonDynamoDBClient Create()
         {
-            if (!string.IsNullOrWhiteSpace(Quartz.DynamoDB.DynamoConfiguration.ServiceUrl))
-            {
+            //if (true)//!string.IsNullOrWhiteSpace(Quartz.DynamoDB.DynamoConfiguration.ServiceUrl))
+            //{
                 // First, set up a DynamoDB client for DynamoDB Local
                 AmazonDynamoDBConfig ddbConfig = new AmazonDynamoDBConfig();
                 ddbConfig.ServiceURL = Quartz.DynamoDB.DynamoConfiguration.ServiceUrl;
 
                 return new AmazonDynamoDBClient(ddbConfig);
-            }
+            //}
 
             // If no url in the config, use the profile and region from the config.
-            return new AmazonDynamoDBClient();
+            //return new AmazonDynamoDBClient();
         }
     }
 }

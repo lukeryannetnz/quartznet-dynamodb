223ca0c304d0c2f27d017375a98bc9bdb049f83a
Dhruv Dhir <dhruvdhir11@gmail.com>
2016-11-09 21:14:04 +1300

    Try default timezone

--- src/QuartzNET-DynamoDB.Tests/Unit/TriggerSerialisationCronTriggerTests.cs
+++ src/QuartzNET-DynamoDB.Tests/Unit/TriggerSerialisationCronTriggerTests.cs
@@ -39,7 +39,7 @@ namespace Quartz.DynamoDB.Tests.Unit
             var jobKey = new JobKey("test");
             var trigger = (CronTriggerImpl)TriggerBuilder.Create()
                 .ForJob(jobKey)
-                .WithCronSchedule("0 0 5 ? * *", x => x.InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time")))
+                .WithCronSchedule("0 0 5 ? * *")
                 .Build();
             return trigger;
         }

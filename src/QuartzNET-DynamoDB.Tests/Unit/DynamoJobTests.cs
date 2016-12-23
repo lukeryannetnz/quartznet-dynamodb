6d2afb9d605d39a878644254292c7f37a24d19b7
Luke Ryan <hello@lukeryan.net.nz>
2016-12-23 17:56:31 +1300

    Unit test for job state

---
+++ src/QuartzNET-DynamoDB.Tests/Unit/DynamoJobTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Quartz.DynamoDB.DataModel;
+using Xunit;
+
+namespace Quartz.DynamoDB.Tests
+{
+    /// <summary>
+    /// Contains tests for the dynamo job class.
+    /// </summary>
+    public class DynamoJobTests
+    {
+        public DynamoJobTests()
+        {
+        }
+
+        /// <summary>
+        /// Tests that a new DynamoJob object has its state set correctly.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void InitialisedState()
+        {
+            var sut = new DynamoJob();
+
+            Assert.Equal(DynamoJobState.Active, sut.State);
+        }
+    }
+}

6d7d116f2a008978f9d67944805b5abecd4348ba
Luke Ryan <hello@lukeryan.net.nz>
2016-12-31 17:05:38 +0800

    Don't serialise tests.

--- mono-integrationtest.sh
+++ mono-integrationtest.sh
@@ -1,1 +1,1 @@
-mono src/packages/xunit.runner.console.2.1.0/tools/xunit.console.exe src/QuartzNET-DynamoDB.Tests/bin/release/QuartzNET-DynamoDB.Tests.dll -verbose -trait "category=integration"  -parallel none -serialize
+mono src/packages/xunit.runner.console.2.1.0/tools/xunit.console.exe src/QuartzNET-DynamoDB.Tests/bin/release/QuartzNET-DynamoDB.Tests.dll -verbose -trait "category=integration"

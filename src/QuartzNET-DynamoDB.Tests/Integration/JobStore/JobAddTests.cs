c8dc6aca119dbabc5eaa7b865700267d3ce519f2
Luke Ryan <hello@lukeryan.net.nz>
2016-12-31 17:07:12 +0800

    Fix whitespace.

--- src/QuartzNET-DynamoDB.Tests/Integration/JobStore/JobAddTests.cs
+++ src/QuartzNET-DynamoDB.Tests/Integration/JobStore/JobAddTests.cs
@@ -9,89 +9,89 @@ using Xunit;
 
 namespace Quartz.DynamoDB.Tests.Integration.JobStore
 {
-	/// <summary>
-	/// Contains tests related to the Addition of Jobs and Job Groups.
-	/// </summary>
+    /// <summary>
+    /// Contains tests related to the Addition of Jobs and Job Groups.
+    /// </summary>
     public class JobAddTests : IDisposable
-	{
+    {
         private readonly DynamoDB.JobStore _sut;
         private readonly DynamoClientFactory _testFactory;
 
-		public JobAddTests ()
-		{
-			_testFactory = new DynamoClientFactory();
+        public JobAddTests()
+        {
+            _testFactory = new DynamoClientFactory();
             _sut = _testFactory.CreateTestJobStore();
-            var signaler = new Quartz.DynamoDB.Tests.Integration.RamJobStoreTests.SampleSignaler ();
-			var loadHelper = new SimpleTypeLoadHelper ();
+            var signaler = new Quartz.DynamoDB.Tests.Integration.RamJobStoreTests.SampleSignaler();
+            var loadHelper = new SimpleTypeLoadHelper();
 
-			_sut.Initialize (loadHelper, signaler);
-		}
+            _sut.Initialize(loadHelper, signaler);
+        }
 
-		/// <summary>
-		/// Tests that when CheckExists is called for a job that exists, it returns true.
-		/// </summary>
-		[Fact]
-		[Trait ("Category", "Integration")]
-		public void CheckExistsWhenJobExsits ()
-		{
-			JobDetailImpl detail = TestJobFactory.CreateTestJob ();
-			_sut.StoreJob (detail, false);
+        /// <summary>
+        /// Tests that when CheckExists is called for a job that exists, it returns true.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void CheckExistsWhenJobExsits()
+        {
+            JobDetailImpl detail = TestJobFactory.CreateTestJob();
+            _sut.StoreJob(detail, false);
 
-			bool result = _sut.CheckExists (detail.Key);
-			Assert.True (result);
-		}
+            bool result = _sut.CheckExists(detail.Key);
+            Assert.True(result);
+        }
 
-		/// <summary>
-		/// Tests that after storing a new job, that job can be retrieved
-		/// with the same name, group and type.
-		/// </summary>
-		[Fact]
-		[Trait ("Category", "Integration")]
-		public void StoreNewJob ()
-		{
-			JobDetailImpl detail = TestJobFactory.CreateTestJob ();
-			_sut.StoreJob (detail, false);
+        /// <summary>
+        /// Tests that after storing a new job, that job can be retrieved
+        /// with the same name, group and type.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void StoreNewJob()
+        {
+            JobDetailImpl detail = TestJobFactory.CreateTestJob();
+            _sut.StoreJob(detail, false);
 
-			var result = _sut.RetrieveJob (new JobKey (detail.Name, detail.Group));
+            var result = _sut.RetrieveJob(new JobKey(detail.Name, detail.Group));
 
-			Assert.NotNull (result);
-			Assert.Equal (detail.Name, result.Key.Name);
-			Assert.Equal (detail.Group, result.Key.Group);
-			Assert.Equal (typeof (NoOpJob), result.JobType);
-		}
+            Assert.NotNull(result);
+            Assert.Equal(detail.Name, result.Key.Name);
+            Assert.Equal(detail.Group, result.Key.Group);
+            Assert.Equal(typeof(NoOpJob), result.JobType);
+        }
 
-		/// <summary>
-		/// Tests that after storing an exsting job, that job is updated if the replaceExisting param is passed as true.
-		/// </summary>
-		[Fact]
-		[Trait ("Category", "Integration")]
-		public void StoreExistingJobOverwrite ()
-		{
-			JobDetailImpl detail = TestJobFactory.CreateTestJob ();
-			detail.Description = "Original";
+        /// <summary>
+        /// Tests that after storing an exsting job, that job is updated if the replaceExisting param is passed as true.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void StoreExistingJobOverwrite()
+        {
+            JobDetailImpl detail = TestJobFactory.CreateTestJob();
+            detail.Description = "Original";
 
-			_sut.StoreJob (detail, false);
+            _sut.StoreJob(detail, false);
 
-			detail.Description = "Updated";
-			_sut.StoreJob (detail, true);
+            detail.Description = "Updated";
+            _sut.StoreJob(detail, true);
 
-			var result = _sut.RetrieveJob (new JobKey (detail.Name, detail.Group));
+            var result = _sut.RetrieveJob(new JobKey(detail.Name, detail.Group));
 
-			Assert.Equal ("Updated", result.Description);
-		}
+            Assert.Equal("Updated", result.Description);
+        }
 
-		/// <summary>
-		/// Tests that after storing an exsting job, an exception is thrown if that job is updated and the replaceExisting param is passed as False.
-		/// </summary>
-		[Fact]
-		[Trait ("Category", "Integration")]
-		public void StoreExistingJobDontOverwriteThrows ()
-		{
-			JobDetailImpl detail = TestJobFactory.CreateTestJob ();
-			_sut.StoreJob (detail, false);
+        /// <summary>
+        /// Tests that after storing an exsting job, an exception is thrown if that job is updated and the replaceExisting param is passed as False.
+        /// </summary>
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void StoreExistingJobDontOverwriteThrows()
+        {
+            JobDetailImpl detail = TestJobFactory.CreateTestJob();
+            _sut.StoreJob(detail, false);
 
-			Assert.Throws<ObjectAlreadyExistsException> (() => _sut.StoreJob (detail, false));
-		}
+            Assert.Throws<ObjectAlreadyExistsException>(() => _sut.StoreJob(detail, false));
+        }
 
         #region IDisposable implementation
 
@@ -123,6 +123,6 @@ namespace Quartz.DynamoDB.Tests.Integration.JobStore
         }
 
         #endregion
-	}
+    }
 }
 

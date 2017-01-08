ef613b322efb9263f7b806bf3ebbd2917d5b9dd8
Luke Ryan <hello@lukeryan.net.nz>
2017-01-08 20:53:22 +1300

    Move JobStore cleanup to abstract class.

---
+++ src/QuartzNET-DynamoDB.Tests/Integration/JobStore/JobStoreIntegrationTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Quartz.DynamoDB.Tests.Integration.JobStore
+{
+    /// <summary>
+    /// Abstract class that provides common dynamo db cleanup for JobStore integration testing.
+    /// </summary>
+    public abstract class JobStoreIntegrationTest : IDisposable
+    {
+        protected DynamoDB.JobStore _sut;
+
+        protected DynamoClientFactory _testFactory;
+
+        #region IDisposable implementation
+
+        bool _disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _testFactory.CleanUpDynamo();
+
+                    if (_sut != null)
+                    {
+                        _sut.Dispose();
+                    }
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+
+        #endregion
+    }
+}
